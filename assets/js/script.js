6de9f8b2ac71f323f1d85e124ac3e621f7cb7805
Humbertov374 <humbertov374@gmail.com>
2022-11-17 12:40:32 -0700

    added opening layout, started with start quiz/ scores buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,21 @@ var a1;
 var a2;
 var a3;
 var a4;
+
+//layout
+function opening() {
+    highscore.style.visibility = " hidden"
+    a1 = document.createElement("button")
+    a1.innerHTML = "Start Quiz!";
+    document.body.children[4].appendChild(a1);
+    a1.addEventListener("click", function () {
+        question1();
+    });
+    a2 = document.createElement("button");
+a2.innerHTML = "Scores!";
+document.body.children[4].appendChild(a2);
+a2.addEventListener("click", function () {
+    scorepage();
+});
+}
+
